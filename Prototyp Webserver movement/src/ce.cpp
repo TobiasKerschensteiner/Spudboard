16edfddf775a624c926e948258ba511c71b36728
Celineernst <120719407+Celineernst@users.noreply.github.com>
2023-10-20 08:57:53 +0200

    Robo kann ich vier verschiedene richtungen fahren

--- Prototyp Webserver movement/src/ce.cpp
+++ Prototyp Webserver movement/src/ce.cpp
@@ -1,129 +1,86 @@
-#include <Arduino.h>
-#include <Stepper.h>
-
-/* 
-//einzeln youtube video (https://www.youtube.com/watch?v=20lSgKkJVXQ)
-#define Step 7
-#define Dir 6
-
-#define MS1 4
-#define MS2 3
-#define MS3 2
-
-void setup()
-{
-    pinMode(Step, OUTPUT);
-    pinMode(Dir, OUTPUT);
-
-    pinMode(MS1, OUTPUT);
-    pinMode(MS2, OUTPUT);
-    pinMode(MS3, OUTPUT);
-}
-
-void loop()
-{
-    digitalWrite(Dir, HIGH);
-    digitalWrite(MS1, HIGH);
-    digitalWrite(MS2, HIGH);
-    digitalWrite(MS3, HIGH);
-
-    digitalWrite(Step, HIGH);
-    delayMicroseconds(30);
-    digitalWrite(Step, LOW);
-    delayMicroseconds(30);
-}
-*/
-
-//oder beispiel internet (https://funduino.de/nr-15-schrittmotor)
-
-int SPU = 2038; // Schritte pro Umdrehung.
-Stepper Motor(SPU, 3,5,4,6); // Der Schrittmotor erhält die Bezeichnung "Motor" und es wird angegeben an welchen Pins der Motor angeschlossen ist.
-
-void setup() 
-{
-Motor.setSpeed(5); // Angabe der Geschwindigkeit in Umdrehungen pro Minute.
-}
-
-void loop() {
-Motor.step(2038); // Der Motor macht 2048 Schritte, das entspricht einer Umdrehung.
-delay(1000); 
-Motor.step(-2038); // Der Motor macht durch das Minuszeichen 2048 Schritte in die andere Richtung.
-delay(1000); 
-}
-
 /*
-//freestyle
+Hilfe:
+https://cool-web.de/arduino/multi-function-shield-step-motor.htm
+https://www.airspayce.com/mikem/arduino/AccelStepper/
+AHHHHHHHHHHHHHHH
+*/
+#include <Arduino.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
 
-// Motoren definieren
-//Motor 1
-#define MS11 = 3;
-#define in11 = 4;
-#define in22 = 5;
+//pin belegung
+AccelStepper stepper1(AccelStepper::FULL4WIRE, 17, 18, 5, 16); //(Wenn + rück wenn - vor)
+AccelStepper stepper2(AccelStepper::FULL4WIRE, 26, 14, 27, 12); //(Wenn + rück wenn - vor)
 
-//Motor 2
-#define MS22 = 8;
-#define in33 = 7;
-#define in44 = 6;
+MultiStepper steppers;
 
-void setup()
-{
-    pinMode(MS11, OUTPUT);
-    pinMode(MS22, OUTPUT);
-    pinMode(in11, OUTPUT);
-    pinMode(in22, OUTPUT);
-    pinMode(in33, OUTPUT);
-    pinMode(in44, OUTPUT);
+void setup() {
+  Serial.begin(9600);
+
+  // Max Geschwindigkeit soll nicht mehr als 2000 betragen
+  stepper1.setMaxSpeed(200);
+  stepper2.setMaxSpeed(200);
+
+  //sanfter anlauf
+  stepper1.setAcceleration(100);
+  stepper2.setAcceleration(100);
+
+  //Stepper zusammen fügen. Geht bis zu Max 10
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
+
 }
 
-void Forward()
+
+//Vorwärtsfahren
+void moveForward()
 {
-    //Motor 1
-    digitalWrite(in11, LOW);
-    digitalWrite(in22, HIGH);
-    
-    //Motor 2
-    digitalWrite(in33, LOW);
-    digitalWrite(in44, HIGH);
+    int steps = 200; // Anzahl der schritte
+    stepper1.move(-steps);
+    stepper2.move(-steps);
+    stepper1.run();
+    stepper2.run();
 }
 
-void Right()
+
+//Rechtedrehung 90°
+void turnRight()
 {
-   //Motor 1
-    digitalWrite(in11, LOW);
-    digitalWrite(in22, HIGH);
-    
-    //Motor 2
-    digitalWrite(in33, HIGH);
-    digitalWrite(in44, LOW);
+    int steps = 200; // Anzahl der Schritte um 90° nach rechts zu drehen
+    stepper1.move(steps);
+    stepper2.move(-steps);
+    stepper1.run();
+    stepper2.run();
 }
 
-void Left()
+//Linksdrehung 90°
+void turnLeft()
 {
-    //Motor 1
-    digitalWrite(in11, HIGH);
-    digitalWrite(in22, LOW);
-    
-    //Motor 2
-    digitalWrite(in33, LOW);
-    digitalWrite(in44, HIGH);
+
+    int steps = 200; //Anzahl der Schritte um 90° nach links zu drehen 
+    stepper1.move(-steps);
+    stepper2.move(steps);
+    stepper1.run();
+    stepper2.run();
 }
 
-void Backward() //falls von nöten
+//Rückwärtsfahren falls von nöten
+void moveBackward()
 {
-    //Motor 1
-    digitalWrite(in11, HIGH);
-    digitalWrite(in22, LOW);
-    
-    //Motor 2
-    digitalWrite(in33, HIGH);
-    digitalWrite(in44, LOW);
-} 
+    int steps = 50000; //Anzahl der Schritte
+    stepper1.move(steps);
+    stepper2.move(steps);
+    stepper1.run();
+    stepper2.run();
+}
 
+
+//Test im kreis fahren
 void loop()
 {
-    Forward(); //Roboter fährt vorwärts
-    delay(1000); //Dauer für größe des Kreises
-    Right(); //Roboter dreht sich
-    delay(500); //Dauer für die Krümmung
+
+moveForward();
+
+ 
+
 }
-*/
