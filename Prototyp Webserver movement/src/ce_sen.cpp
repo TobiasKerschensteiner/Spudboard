aaad3204d7d686bc9d5ff1d8af85a97809113a1e
Alpakhan <al.pay@hotmail.de>
2024-01-26 11:56:59 +0100

    Last update

--- Prototyp Webserver movement/src/ce_sen.cpp
+++ Prototyp Webserver movement/src/ce_sen.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-#define FULLSTEP 4
 #define HALFSTEP 8
 
-const float SteppDegree = 11.32;
+const float SteppDegree = 11.32; // Schritte pro Grad
 int maxstepperSpeed = 2000;
 int stepperSpeed = 200;
 int stepperBesch = 100;
@@ -14,8 +13,8 @@ int sensor = 15;
 AccelStepper stepper1(HALFSTEP, 17, 18, 5, 16);  
 AccelStepper stepper2(HALFSTEP, 26, 14, 27, 12);
 
-enum MovementState { FORWARD, TURNING, STOPPED };
-MovementState currentState = STOPPED;
+enum MovementState { FORWARD, TURNING, AFTER_TURN, STOPPED };
+MovementState currentState = FORWARD;
 
 void setup(void) {
   delay(3000);
@@ -31,33 +30,28 @@ void setup(void) {
   stepper2.setCurrentPosition(0);
 
   pinMode(sensor, INPUT);
+
+  // Starte mit Vorwärtsbewegung
+  stepper1.moveTo(stepper1.currentPosition() + 2000); 
+  stepper2.moveTo(stepper2.currentPosition() + 2000);
 }
 
-void turn(int direction) {
-  // Richtung: 1 für Rechtsdrehung, -1 für Linksdrehung
-  if (currentState != TURNING) {
-    float degree = 90; // Winkel für Drehung
-    float moveRev = degree * SteppDegree;
-    stepper1.moveTo(stepper1.currentPosition() + direction * moveRev);
-    stepper2.moveTo(stepper2.currentPosition() - direction * moveRev);
-    currentState = TURNING;
-  }
+void turn() {
+  // Stoppe Motoren vor der Drehung
+  stepper1.stop();
+  stepper2.stop();
+
+  float degree = 180; // Winkel für 90 Grad Drehung
+  float moveRev = degree * SteppDegree;
+  stepper1.moveTo(stepper1.currentPosition() + moveRev);
+  stepper2.moveTo(stepper2.currentPosition() - moveRev);
+  currentState = TURNING;
 }
 
-void forward() {
-  if (currentState != FORWARD) {
-    stepper1.moveTo(stepper1.currentPosition() + 2000); // Wert für Vorwärtsbewegung
-    stepper2.moveTo(stepper2.currentPosition() + 2000);
-    currentState = FORWARD;
-  }
-}
-
-void stopMotors() {
-  if (currentState != STOPPED) {
-    stepper1.stop();
-    stepper2.stop();
-    currentState = STOPPED;
-  }
+void forwardAfterTurn() {
+  stepper1.moveTo(stepper1.currentPosition() + 1000); 
+  stepper2.moveTo(stepper2.currentPosition() + 1000);
+  currentState = AFTER_TURN;
 }
 
 void loop(void) {
@@ -66,13 +60,20 @@ void loop(void) {
   stepper1.run();
   stepper2.run();
 
-  if (sensorValue == 0 && !stepper1.isRunning() && !stepper2.isRunning()) {
-    forward();
-  } else if (sensorValue == 1) {
-    if (currentState == FORWARD) {
-      stopMotors();
-      delay(100);
-      turn(1); // Ändern Sie den Wert auf -1 für eine Linksdrehung
+  // Prüfen, ob eine Aktion erforderlich ist
+  if (!stepper1.isRunning() && !stepper2.isRunning()) {
+    switch (currentState) {
+      case FORWARD:
+        if (sensorValue == 1) { // Sensor ausgelöst
+          turn();
+        }
+        break;
+      case TURNING:
+        forwardAfterTurn();
+        break;
+      case AFTER_TURN:
+        // Hier könnte eine weitere Aktion oder Bewegung implementiert werden
+        break;
     }
   }
 }
