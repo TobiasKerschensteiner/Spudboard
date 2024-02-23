c32b3495711cbca8792e05c8671843eb61ee8043
TobiasKerschensteiner <120680330+TobiasKerschensteiner@users.noreply.github.com>
2024-02-23 12:11:50 +0100

    Charge variable hinzugefügt
    Variable die angibt ob Mobbot geladen wird hinzugefügt

--- Prototyp Webserver movement/src/Tobi.cpp
+++ Prototyp Webserver movement/src/Tobi.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 
 int stepCounter;
@@ -71,7 +72,7 @@ void loop(){
         digitalWrite(DIRR,HIGH);
         moveF();
         digitalWrite(Wisch,HIGH); 
-        Serial.println("Straight");
+        //Serial.println("Straight");
       
     }
     else
